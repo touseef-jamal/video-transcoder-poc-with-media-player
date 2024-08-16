23fe8e871cd4d4cbd9815098e4a6bf1f00975caa
Mohd Touseef Jamal <touseef7294@gmail.com>
2024-08-16 19:21:49 +0530

    simplified video player for YouTube

--- frontend/src/VideoPlayer.jsx
+++ frontend/src/VideoPlayer.jsx
@@ -1,26 +1,46 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useRef } from "react"
 import videojs from "video.js"
 
 import "video.js/dist/video-js.css"
 
-const VIDEO_BASE_URL = "http://localhost:2000/hls-output"
+const BASE_URL = "http://localhost:2000/hls-output"
 
 const VideoPlayer = ({ videoId }) => {
     const videoRef = useRef(null)
     const playerRef = useRef(null)
-    const [availableResolutions, setAvailableResolutions] = useState([])
 
-    const masterFileSource = {
-        src: `${VIDEO_BASE_URL}/${videoId}/index.m3u8`,
-        type: "application/x-mpegURL",
-    }
+    const masterFileSrc = `${BASE_URL}/${videoId}/index.m3u8`
+
+    const availableResolutions = [
+        {
+            label: "360p",
+            src: `${BASE_URL}/${videoId}/360p/index.m3u8`,
+        },
+        {
+            label: "480p",
+            src: `${BASE_URL}/${videoId}/480p/index.m3u8`,
+        },
+        {
+            label: "720p",
+            src: `${BASE_URL}/${videoId}/720p/index.m3u8`,
+        },
+        {
+            label: "1080p",
+            src: `${BASE_URL}/${videoId}/1080p/index.m3u8`,
+        },
+    ]
 
     const options = {
         autoplay: true,
         controls: true,
         responsive: true,
         fluid: true,
-        sources: [masterFileSource],
+        sources: [
+            {
+                src: masterFileSrc,
+                type: "application/x-mpegURL",
+            },
+        ],
     }
 
     const handleResolutionChange = (index) => {
@@ -31,68 +51,25 @@ const VideoPlayer = ({ videoId }) => {
             src: availableResolutions[index].src,
             type: "application/x-mpegURL",
         })
-
         player.ready(() => {
             player.currentTime(currentTime)
         })
     }
 
     useEffect(() => {
-        const fetchMasterPlaylist = async () => {
-            const response = await fetch(masterFileSource.src)
-            const masterFileContent = await response.text()
-            const resolutions = parseMasterPlaylist(masterFileContent)
-            setAvailableResolutions(resolutions)
-        }
-
-        const parseMasterPlaylist = (text) => {
-            const resolutions = []
-            if (text.includes("360p/index.m3u8")) {
-                resolutions.push({
-                    src: `${VIDEO_BASE_URL}/${videoId}/360p/index.m3u8`,
-                    label: "360p",
-                })
-            }
-            if (text.includes("480p/index.m3u8")) {
-                resolutions.push({
-                    src: `${VIDEO_BASE_URL}/${videoId}/480p/index.m3u8`,
-                    label: "480p",
-                })
-            }
-            if (text.includes("720p/index.m3u8")) {
-                resolutions.push({
-                    src: `${VIDEO_BASE_URL}/${videoId}/720p/index.m3u8`,
-                    label: "720p",
-                })
-            }
-            if (text.includes("1080p/index.m3u8")) {
-                resolutions.push({
-                    src: `${VIDEO_BASE_URL}/${videoId}/1080p/index.m3u8`,
-                    label: "1080p",
-                })
-            }
-            return resolutions
-        }
-
-        fetchMasterPlaylist()
-    }, [videoId])
-
-    useEffect(() => {
         // Make sure Video.js player is only initialized once
         if (!playerRef.current) {
-            // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
             const videoElement = document.createElement("video-js")
-
             videoElement.classList.add("vjs-big-play-centered")
             videoRef.current.appendChild(videoElement)
 
             const player = (playerRef.current = videojs(videoElement, options))
 
-            // Append buttons for changing resolution
+            // Append video resolution button
 
-            availableResolutions.forEach((source, index) => {
+            availableResolutions.forEach((resolution, index) => {
                 player.getChild("ControlBar").addChild("button", {
-                    controlText: source.label,
+                    controlText: resolution.label,
                     className: "vjs-visible-text",
                     clickHandler: () => {
                         handleResolutionChange(index)
@@ -102,23 +79,26 @@ const VideoPlayer = ({ videoId }) => {
         } else {
             const player = playerRef.current
             player.autoplay(options.autoplay)
-            player.src([masterFileSource])
+            player.src([options.sources])
         }
+    }, [videoRef])
+
+    // Dispose the Video.js player when the functional component unmounts
+    useEffect(() => {
+        const player = playerRef.current
 
         return () => {
-            const player = playerRef.current
             if (player && !player.isDisposed()) {
                 player.dispose()
                 playerRef.current = null
             }
         }
-    }, [videoId, videoRef, availableResolutions])
+    }, [playerRef])
 
     return (
-        <div data-vjs-player={true}>
+        <div data-vjs-player>
             <div ref={videoRef} />
         </div>
     )
 }
-
 export default VideoPlayer
